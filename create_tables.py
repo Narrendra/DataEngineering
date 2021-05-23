0ce06113d4730b2d4221a8f8df670526ce02c5fc
Narrendra <narendrapendem11@gmail.com>
2021-05-23 00:16:01 -0700

    Updated create table code

--- create_tables.py
+++ create_tables.py
@@ -1,1 +1,19 @@
-#This file creates the tables in postgres db
+#This file creates the tables in SQL SERVER
+# import pyodbc
+import pyodbc
+
+#Create a connection with MS SQL SERVER 
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-DC1BM20;'
+                      'Database=master;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM student')
+
+for row in cursor:
+    print(row)
+
+# def checkIfDBExists(dbName)
+
